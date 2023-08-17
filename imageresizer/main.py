d59035ee3cdac7da3675d1eace516502347dd519
surbhi12code <103835543+surbhi12code@users.noreply.github.com>
2023-08-17 21:45:57 +0530

    Add files via upload

---
+++ imageresizer/main.py
@@ -0,0 +1,22 @@
+import cv2
+source="taylor.jpeg"
+destination="newImage.png"
+scale_percent = 50
+src=cv2.imread(source,cv2.IMREAD_UNCHANGED)
+# cv2.imshow("title",src)
+
+
+
+
+#calculate the 50 percent of original dimensions
+new_width = int(src.shape[1] * scale_percent / 100)
+new_height = int(src.shape[0] * scale_percent / 100)
+
+# dsize
+dsize = (new_width, new_height)
+
+# resize image
+output = cv2.resize(src, dsize)
+
+cv2.imwrite(destination,output)
+cv2.waitKey(0)
